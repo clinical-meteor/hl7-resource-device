f1f7df4906f731817db680dfd4652a5d7a627407
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-01 22:58:05 -0500

    circle 2.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-device',
-  version: '1.2.3',
+  version: '1.2.4',
   summary: 'HL7 FHIR Resource - Device',
   git: 'https://github.com/clinical-meteor/hl7-resource-device',
   documentation: 'README.md'
