3dc85b080864b539aee85bd1ce67573882defd39
Abigail Watson <abigailwatson@Abigails-Mac-mini-317772.local>
2018-07-01 19:38:32 -0500

    clinical:alert@2.1.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-device',
-  version: '1.2.1',
+  version: '1.2.2',
   summary: 'HL7 FHIR Resource - Device',
   git: 'https://github.com/clinical-meteor/hl7-resource-device',
   documentation: 'README.md'
